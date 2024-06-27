2f86e30a4605f210fec9e7bdd4d9af9abcd62904
iamabhishek <itsabhishek9534@gmail.com>
2024-06-27 21:20:03 +0530

    added

--- amazon/server/app.js
+++ amazon/server/app.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+require("./dp/conn");
 const app = express();
+const Products = require("./models/productsSchema");
+
+
+const DefaultData = require("./defaultdata");
 
 app.listen(3000,() =>{
     console.log("Listening to the port 3000");
 });
+
+DefaultData();
